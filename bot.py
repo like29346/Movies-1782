26bbe23c87a82599df05ac9bf216566f4826352f
like29346 <like29346@gmail.com>
2025-10-16 12:24:55 +0530

    Update DATABASE_URL configuration key

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ LOG_CHANNEL = int(get_config("LOG_CHANNEL")) if get_config("1002630246729") else
 UPDATE_CHANNEL = get_config("1003103870573")
 
 # Optional Keys
-DATABASE_URL = get_config("movies123", required=False)
+DATABASE_URL = get_config("Database movies1782🎬", required=False)
 USER_SESSION_STRING = get_config("1BVtsOKEBu7HCXYkQunrqrwAFCE6ivknaOrXWfguNisN_T8SGFVo26zIsfahH-1boT_a46rYpWy6-vPjRVr2mnKAXIvaZpTmgijXz8_au0GjW_M7cyCiswNOEFqRWHXiOSsgf6Ltzar7pDBr_fJDz_wSRBKCdM0GU1f6mUioRYukcvplKFYfdg8W4b_Hdw2fW_IXgl0SPC32gWOeTkRKDiAx04AFTT9Zh9DftIR7PvbsV76xa4BBi-FYuGPUQiSaqgioZ5RYWjQ-ZAJb_fhXtcwFiJDgOqnfHKvR5OP8mm4-EC3WuLWFANuicRNq2N23NS7YZtEvMqedDx9p3TlX92voRvM31tzk=", required=False)
 
 if __name__ == "__main__":
